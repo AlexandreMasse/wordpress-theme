a885ec7e29d5ae2a603786b9cc50d7ef905ce8a2
Alexandre Masse <alexandre.masse94@gmail.com>
2017-02-16 14:36:00 -0500

    first commit + add header footer index style and screenshot

---
+++ footer.php
@@ -0,0 +1,15 @@
+    </div> <!-- fermeture de la div #contenu -->
+
+    <footer>
+        <p><?php bloginfo('description'); ?></p>
+        <?php wp_footer(); ?>
+    </footer>
+</body>
+</html>
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Alex
+ * Date: 16/02/2017
+ * Time: 13:45
+ */
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php get_header(); ?> <!-- Appel à l’entête -->
+
+<main id="articles">
+    <!-- teste s'il y a des posts-->
+    <?php if(have_posts()) : ?>
+        <!-- boucle tant qu'il y a des articles -->
+        <!-- chaque article lu, la boucle récupère ses informations grâce à la fonction the_post()-->
+        <?php while(have_posts()) : the_post(); ?>
+            <!-- Chaque article est inséré dans un élément article -->
+            <article>
+                <header class="titre-article">
+                    <h2>
+                        <a href="<?php the_permalink(); ?>">
+                            <!-- Le titre de l’article est affiché dans un élément header à l’aide de la fonction the_title() -->
+                            <?php the_title(); ?>
+                        </a>
+                    </h2>
+                </header>
+                <!-- Le contenu de l’article est affiché avec la fonction the_content().-->
+                <?php the_content(); ?>
+            </article>
+        <?php endwhile; ?>
+    <?php endif; ?>
+</main>
+
+<?php get_footer(); ?> <!-- Appel au footer -->
